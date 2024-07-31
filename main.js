74b346ca27a547415e789151fd8f55e506a5d518
JungSeoro <jsr@jungseoro.com>
2024-07-31 18:00:06 +0900

    Modify titlebar

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hidden',
+    resizable: true,
+    maximizable: true,
+    minimizable: true,
+    frame: false,
+    backgroundColor: '#ffffff',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
